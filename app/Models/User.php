a0bd734495f48aa0530be2f7fe224fc1d69d6c12
raheel0316 <raheelanwaar0316@gmail.com>
2024-01-02 12:58:02 -0800

    adding template

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'balance',
+        'role',
+        'status',
+        'referral',
     ];
 
     /**
