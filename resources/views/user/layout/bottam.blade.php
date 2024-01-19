2256f3994924175104ea61338e152d64a44099f3
raheel0316 <raheelanwaar0316@gmail.com>
2024-01-19 12:05:28 -0800

    color black

--- resources/views/user/layout/bottam.blade.php
+++ resources/views/user/layout/bottam.blade.php
@@ -3,34 +3,34 @@
         <ul class="tf-navigation-bar">
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
-                    href="{{ route('User.Dashboard') }}">
-                    <img src="{{ asset('assets/green_home.png') }}" style="color:black;height: 30px;width:30px"> Home
+                    href="{{ route('User.Dashboard') }}" style="color: black;">
+                    <img src="{{ asset('assets/green_home.png') }}" style="height: 30px;width:30px"> Home
                 </a>
             </li>
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
-                    href="{{ route('User.History') }}">
+                    href="{{ route('User.History') }}" style="color: black;">
                     <img src="{{ asset('assets/green_history.png') }}" style="height: 30px;width:30px">
                     History
                 </a>
             </li>
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
-                    href="{{ route('User.Plan.Status') }}">
-                    <img src="{{ asset('assets/green_trade.png') }}" style="color:black;height: 30px;width:30px">
+                    href="{{ route('User.Plan.Status') }}" style="color: black;">
+                    <img src="{{ asset('assets/green_trade.png') }}" style="height: 30px;width:30px">
                     Trade
                 </a>
             </li>
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
-                    href="{{ route('User.Team.View') }}">
-                    <img src="{{ asset('assets/green_team.png') }}" style="color:black;height: 30px;width:30px">Team
+                    href="{{ route('User.Team.View') }}" style="color: black;">
+                    <img src="{{ asset('assets/green_team.png') }}" style="height: 30px;width:30px">Team
                 </a>
             </li>
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
-                    href="{{ route('User.Profile') }}">
-                    <img src="{{ asset('assets/green_profile.png') }}" style="color:black;height: 30px;width:30px">
+                    href="{{ route('User.Profile') }}" style="color: black;">
+                    <img src="{{ asset('assets/green_profile.png') }}" style="height: 30px;width:30px">
                     Profile</a>
             </li>
         </ul>
