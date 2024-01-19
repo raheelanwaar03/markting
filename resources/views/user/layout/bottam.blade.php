f8c9510d33ac9406b2602e52ee9f22e95559d929
raheel0316 <raheelanwaar0316@gmail.com>
2024-01-19 04:09:09 -0800

    icons changed

--- resources/views/user/layout/bottam.blade.php
+++ resources/views/user/layout/bottam.blade.php
@@ -10,27 +10,27 @@
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
                     href="{{ route('User.History') }}">
-                    <img src="{{ asset('assets/chart.png') }}" style="height: 30px;width:30px">
+                    <img src="{{ asset('assets/history.png') }}" style="height: 30px;width:30px">
                     History
                 </a>
             </li>
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
                     href="{{ route('User.Plan.Status') }}">
-                    <img src="{{ asset('assets/checklist.png') }}" style="color:white;height: 30px;width:30px">
+                    <img src="{{ asset('assets/trade.png') }}" style="color:white;height: 30px;width:30px">
                     Trade
                 </a>
             </li>
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
                     href="{{ route('User.Team.View') }}">
-                    <img src="{{ asset('assets/user.png') }}" style="color:white;height: 30px;width:30px">Team
+                    <img src="{{ asset('assets/team.webp') }}" style="color:white;height: 30px;width:30px">Team
                 </a>
             </li>
             <li>
                 <a class="fw_4 d-flex justify-content-center align-items-center flex-column text-light"
                     href="{{ route('User.Profile') }}">
-                    <img src="{{ asset('assets/profile.png') }}" style="color:white;height: 30px;width:30px">
+                    <img src="{{ asset('assets/profile.jpg') }}" style="color:white;height: 30px;width:30px">
                     Profile</a>
             </li>
         </ul>
